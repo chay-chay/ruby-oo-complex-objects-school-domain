d5112d3764c209fa3e012f614fdccbc53730bf84
Chayanit McCormick <chayanit.chaisri@gmail.com>
2020-11-02 12:08:34 -0500

    testing

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,23 @@
 # code here!
+class School
+    attr_accessor :roster, :add_student
+    attr_reader 
+    def initialize(name)
+        @name = name
+        @roster = {}
+    end
+    def add_student(name, grade)
+        roster[grade] ||= []
+        roster[grade] << name
+    end
+    def grade(grade)
+        roster[grade]
+    end
+    def sort
+        sorted = {}
+        roster.each do |grade, student|
+            sorted[grade] = student.sort
+        end
+        sorted
+    end
+end
